c0714dbfec108dd19e03079da6c8a1e66af5ada7
Rahul Gupta <rahulgupta4@practo.com>
2016-04-30 15:26:02 +0530

    Added awesome design & seperately displaying flash messages

--- popup.js
+++ popup.js
@@ -3,16 +3,23 @@ window.addEventListener('DOMContentLoaded', function(){
 	var stagingName = document.getElementById('staging-name');
 	var copyBtn = document.getElementById('btn-copy');
 	var form = document.getElementById('staging-form');
+	var message = document.getElementById('message');
 	
 	var success = function() {
-			copyBtn.innerHTML = 'Copied !';
-			copyBtn.disabled = true;
+		message.innerHTML = 'Copied !';
+		message.classList.add('message--success');
+		message.classList.remove('message--error');
+		copyBtn.disabled = false;
 	};
 	var error = function() {
-			copyBtn.innerHTML = 'Error';
+		message.innerHTML = 'Error !';
+		message.classList.add('message--error');
+		message.classList.remove('message--success');
+		copyBtn.disabled = false;
 	};
 
 	form.onsubmit = function(){
+		copyBtn.disabled = true;
 		copyToClipboard();
 		return false;
 	};
@@ -41,4 +48,37 @@ window.addEventListener('DOMContentLoaded', function(){
 	    	}
 	  	});
 	});
+
+
+
+	/* Input effects */
+	(function() {
+		// trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+		if (!String.prototype.trim) {
+			(function() {
+				// Make sure we trim BOM and NBSP
+				var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+				String.prototype.trim = function() {
+					return this.replace(rtrim, '');
+				};
+			})();
+		}
+		[].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+			// in case the input is already filled..
+			if( inputEl.value.trim() !== '' ) {
+				classie.add( inputEl.parentNode, 'input--filled' );
+			}
+			// events:
+			inputEl.addEventListener( 'focus', onInputFocus );
+			inputEl.addEventListener( 'blur', onInputBlur );
+		} );
+		function onInputFocus( ev ) {
+			classie.add( ev.target.parentNode, 'input--filled' );
+		}
+		function onInputBlur( ev ) {
+			if( ev.target.value.trim() === '' ) {
+				classie.remove( ev.target.parentNode, 'input--filled' );
+			}
+		}
+	})();
 });
